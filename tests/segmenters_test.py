c822fbf9210391e508a642e13e6269a939e7d51c
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2026-04-20 17:44:56 +0700

    test: add texttiling-embeddings tests

--- tests/segmenters_test.py
+++ tests/segmenters_test.py
@@ -1,17 +1,45 @@
 import pytest
 
-from dobermann import DataHandler, DataSet, TextTiling
+from dobermann import DataHandler, DataSet, TextTilingEmbeddings
+
+# TODO: test that texttiling runs
 
 
-# def test_texttiling_segmentations_align():
-#     # the number of sentences must match the sum of the ground truth which must match the sum of the result
-#     samples = DataHandler(DataSet.WIKI_1024).samples
-#     sample = samples[0]
-# 
-#     segmenter = TextTiling(pseudo_sentence_size=5, window_size=3)
-# 
-#     result = segmenter.segment(sample.sentences)
-# 
-#     assert sum(result) == len(
-#         sample.sentences
-#     ), f"Mismatch: pred_total={sum(result)}, num_sentences={len(sample.sentences)}"
+@pytest.fixture
+def segmenter():
+    return TextTilingEmbeddings("all-MiniLM-L6-v2")
+
+
+def test_correct_lengths(segmenter):
+    sentences = [
+        "Cats are animals.",
+        "Dogs are animals.",
+        "Python is a programming language.",
+        "Functions can return values.",
+    ]
+    result = segmenter.segment(sentences)
+
+    assert sum(result.segment_lengths) == len(sentences)
+    # move this to separate
+    assert len(result.metadata["similarities"]) == len(sentences) - 1
+
+
+# TODO: add integration testing with datasets
+
+
+def test_single_sentence(segmenter):
+    result = segmenter.segment(["hello"])
+    assert sum(result.segment_lengths) == 1
+
+
+def test_segments_deterministic(segmenter):
+    sentences = [
+        "Cats are animals.",
+        "Dogs are animals.",
+        "Python is a programming language.",
+        "Functions can return values.",
+    ]
+
+    result1 = segmenter.segment(sentences)
+    result2 = segmenter.segment(sentences)
+    assert result1.segment_lengths == result2.segment_lengths
