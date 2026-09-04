473c72fabc5d418aa5dee47a94cae44c5025e86a
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2026-09-05 00:47:25 +0700

    feat(texttiling): add input processor

--- src/dobermann/segmenters/texttiling/texttiling.py
+++ src/dobermann/segmenters/texttiling/texttiling.py
@@ -1,6 +1,8 @@
+from dobermann.segmenters.abstract import SegmentationResult
 from sentence_transformers import SentenceTransformer
 
 from ...embeddings import Embedder, SentenceTransformerEmbedder
+from ...preprocessors import PreProcessor, IdentityPreProcessor
 from .boundaries import AdaptiveValleyBoundaryDetector, BoundaryDetector
 from .postprocessor import BoundaryToLengthProcessor, PostProcessor
 from .similarity import CosineSimilarity, Similarity
@@ -11,12 +13,14 @@ from .texttiling_embeddings import TextTilingEmbeddings
 class TextTiling(TextTilingEmbeddings):
     def __init__(
         self,
+        pre_processor: PreProcessor | None = None,
         embedder: Embedder | None = None,
         similarity: Similarity | None = None,
         smoother: Smoother | None = None,
         boundary: BoundaryDetector | None = None,
         post_procesor: PostProcessor | None = None,
     ):
+        self.pre_processor = pre_processor or IdentityPreProcessor()
         super().__init__(
             embedder=embedder
             or SentenceTransformerEmbedder(
@@ -27,3 +31,8 @@ class TextTiling(TextTilingEmbeddings):
             boundary=(boundary or AdaptiveValleyBoundaryDetector()),
             post_procesor=(post_procesor or BoundaryToLengthProcessor()),
         )
+
+        def segment(self, input) -> SegmentationResult:
+            sentences = self.pre_processor.process(input)
+            # TODO: add output processor
+            return super().segment(sentences)
