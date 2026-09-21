723f0b5f0a23a44ceeac3a671ed22af6be96f717
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2026-09-21 23:31:36 +0700

    refactor(segmenters): untangle input validation
    This commit separates the input validation from the previously nested
    inheritance logic. The validation now lives inside the preprocessor,
    specifically the IdentityPreProcessor. More sophisticated preprocessors
    should henceforth wrap around said processor.

--- src/dobermann/preprocessors/identity.py
+++ src/dobermann/preprocessors/identity.py
@@ -3,4 +3,13 @@ from .base import PreProcessor
 
 class IdentityPreProcessor(PreProcessor):
     def process(self, sentences: list[str]) -> list[str]:
+        if not isinstance(sentences, list):
+            raise TypeError("sentences must be a list of str")
+
+        if not all(isinstance(sentence, str) for sentence in sentences):
+            raise TypeError("all elements in sentences must be str")
+
+        if not sentences:
+            raise ValueError("sentences must be nonempty")
+
         return sentences
